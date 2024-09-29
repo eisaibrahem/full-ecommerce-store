c45ecef1b3877d44a0ee893820dfdb415aa89e56
eisa ibrahim <68941204+eisaibrahem@users.noreply.github.com>
2024-09-29 23:39:19 +0300

    test

--- actions/get-categories.tsx
+++ actions/get-categories.tsx
@@ -1,13 +1,26 @@
 import { Category } from "@/types";
 
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/categories`;
+// const URL = `https://eistore-dashboard-qhasd7nfm-eisas-projects.vercel.app/api/9986f653-6b60-4fe2-92fe-fe110e4f8441/categories`;
 
 const getCategories = async (): Promise<Category[]> => {
-  const res = await fetch(URL, {
-    cache: "no-store", // هذا يلغي التخزين المؤقت
-  });
+  try {
+    const res = await fetch(URL, {
+      cache: "no-store",
+    });
 
-  return res.json();
+    // إذا كانت الاستجابة غير ناجحة، قم بطباعة الخطأ
+    if (!res.ok) {
+      const errorMessage = await res.text();
+      console.error("Error fetching categories:", errorMessage);
+    }
+
+    // تحويل الاستجابة إلى JSON
+    return await res.json();
+  } catch (error) {
+    console.error("An error occurred:", error);
+    throw error;
+  }
 };
 
 export default getCategories;
--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -55,7 +55,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
     <div
       onClick={handleClick}
       className="bg-white group cursor-pointer rounded-xl hover-zoom 
-      transform hover:scale-105 transition-transform duration-[200] border p-3 space-y-4"
+      transform hover:scale-105 transition-transform duration-200 border p-3 space-y-4"
     >
       {/* Image & actions */}
       <div className="aspect-square rounded-xl bg-gray-100 relative">
