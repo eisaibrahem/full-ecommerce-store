17b9ba46a5bbe22992f7ad5fa1574c6b8fd708be
eisa ibrahim <68941204+eisaibrahem@users.noreply.github.com>
2024-10-03 19:46:13 +0300

    remove share button

--- components/info.tsx
+++ components/info.tsx
@@ -1,77 +1,33 @@
 "use client";
 
 import { Heart, ShoppingCart, Trash2 } from "lucide-react";
-import { useState, useEffect } from "react";
 import Currency from "@/components/ui/currency";
 import { Button } from "@/components/ui/button";
 import { Product } from "@/types";
 import useCart from "@/hooks/use-cart";
 import useWishlist from "@/hooks/use-wishlist";
 import Rating from "./ui/ratings";
-import Image from "next/image";
 
 interface InfoProps {
   data: Product;
 }
 
 const Info: React.FC<InfoProps> = ({ data }) => {
-  const [checker, setChecker] = useState({ share: false, copy: false });
-  const [URL, setURL] = useState<string>("");
   const cart = useCart();
   const wishlist = useWishlist();
 
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      setURL(window.location.href);
-    }
-  }, []);
-
   const onAddToCart = () => cart.addItem(data);
   const onRemoveFromCart = () => cart.removeItem(data.id);
+
+  const isInCart = cart.items.some((item) => item.id === data.id);
+  const isInWishlist = wishlist.items.some((item) => item.id === data.id);
+
   const toggleWishlist = () => {
     if (wishlist.items.some((item) => item.id === data.id)) {
       wishlist.removeItem(data.id);
     } else {
       wishlist.addItem(data);
     }
-  };
-
-  const isInCart = cart.items.some((item) => item.id === data.id);
-  const isInWishlist = wishlist.items.some((item) => item.id === data.id);
-
-  const onShare = () => {
-    setChecker((prev) => ({ ...prev, share: !prev.share }));
-    if (navigator.share) {
-      navigator
-        .share({
-          title: data.name,
-          text: `Check out this product: ${data.name}`,
-          url: URL,
-        })
-        .then(() => console.log("Shared successfully"))
-        .catch((error) => console.error("Error sharing:", error));
-    } else {
-      console.log("Web Share API is not supported in this browser.");
-    }
-  };
-
-  const handleWhatsApp = () =>
-    window.open(`https://wa.me/?text=${encodeURIComponent(URL)}`, "_blank");
-  const handleFaceBook = () =>
-    window.open(
-      `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(URL)}`,
-      "_blank"
-    );
-  const handleInstagram = () =>
-    window.open(
-      `https://www.instagram.com/create/story/?url=${encodeURIComponent(URL)}`,
-      "_blank"
-    );
-
-  const handleCopy = () => {
-    navigator.clipboard.writeText(URL);
-    setChecker((prev) => ({ ...prev, copy: true }));
-    setTimeout(() => setChecker((prev) => ({ ...prev, copy: false })), 2000);
   };
 
   return (
@@ -115,9 +71,7 @@ const Info: React.FC<InfoProps> = ({ data }) => {
             <ShoppingCart size={20} />
           </Button>
         )}
-        <Button onClick={onShare} className="flex items-center gap-x-2">
-          Share
-        </Button>
+
         <Button
           className={`flex items-center gap-x-2 ${
             isInWishlist ? "text-red-600" : "text-background"
@@ -128,63 +82,6 @@ const Info: React.FC<InfoProps> = ({ data }) => {
           <Heart className={isInWishlist ? "fill-red-600" : ""} />
         </Button>
       </div>
-      {checker.share && (
-        <div className="fixed top-0 bottom-0 left-0 right-0 h-[16rem] shadow-lg p-6 w-[85%] bg-white m-auto">
-          <Image
-            width={50}
-            height={50}
-            src="https://cdn-icons-png.flaticon.com/128/2732/2732657.png"
-            className="absolute top-[1%] right-[3%] h-6 w-6 cursor-pointer"
-            alt="close"
-            onClick={() => setChecker((prev) => ({ ...prev, share: false }))}
-          />
-          <div className="flex justify-center items-center gap-8">
-            <input readOnly value={URL} className="border p-2 w-[60%]" />
-            <Image
-              width={50}
-              height={50}
-              src={
-                checker.copy
-                  ? "https://cdn-icons-png.flaticon.com/128/5291/5291043.png"
-                  : "https://cdn-icons-png.flaticon.com/128/126/126498.png"
-              }
-              alt="copy"
-              className={`h-6 w-6 cursor-pointer ${
-                checker.copy ? "cursor-not-allowed" : ""
-              }`}
-              onClick={handleCopy}
-            />
-          </div>
-          <hr className="mt-4 border-black" />
-          <p className="text-center p-4">Share To</p>
-          <div className="flex justify-center items-center gap-4">
-            <Image
-              width={50}
-              height={50}
-              src="https://cdn-icons-png.flaticon.com/128/5968/5968764.png"
-              alt="facebook"
-              onClick={handleFaceBook}
-              className="h-12 w-12 cursor-pointer"
-            />
-            <Image
-              width={50}
-              height={50}
-              src="https://cdn-icons-png.flaticon.com/128/15713/15713420.png"
-              alt="Instagram"
-              onClick={handleInstagram}
-              className="h-12 w-12 cursor-pointer"
-            />
-            <Image
-              width={50}
-              height={50}
-              src="https://cdn-icons-png.flaticon.com/128/15713/15713434.png"
-              alt="WhatsApp"
-              onClick={handleWhatsApp}
-              className="h-12 w-12 cursor-pointer"
-            />
-          </div>
-        </div>
-      )}
     </div>
   );
 };
